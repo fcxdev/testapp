c202758452d4d18c1530527dfa9eab508205d914
fcxdev <devfcx@gmail.com>
2025-06-11 16:45:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bethel App.js</code> and save to reload.
+          <code> Bethel App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
